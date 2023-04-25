c62d28489d3b91eb09ba4e46390c2888bf15f17d
kubaslawski <kubaslawski@gmail.com>
2023-04-25 11:45:43 +0200

    Changed routing - implemented new HOC into each route

--- src/routing/routing.tsx
+++ src/routing/routing.tsx
@@ -2,14 +2,22 @@ import React from 'react';
 import {Route, Routes} from "react-router-dom";
 // routes
 import {paths} from "./paths";
+// hoc
+import WithAuth from "../hoc/with-auth/with-auth";
 
 const Routing: React.FC = () => {
 
 	return (
 			<Routes>
 				{Object.entries(paths).map(([key, value]) => {
+					const WrappedComponent = WithAuth(value.element);
 					return (
-						<Route key={key} path={value.path} element={<value.element/>}/>
+						// <Route key={key} path={value.path} element={<value.element/>}/>
+						<Route
+							key={key}
+							path={value.path}
+							element={<WrappedComponent/>}
+						/>
 					)
 				})}
 			</Routes>
